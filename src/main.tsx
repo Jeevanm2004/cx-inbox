37fcd8eb7a72f4b937760c7255f2ff36c25c7641
Jeevan M <jeevanm.bit@gmail.com>
2026-06-29 10:14:22 +0530

    fix(msw): enable service worker in all environments for Vercel demo

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,8 @@ import './index.css'
 import App from './App.tsx'
 
 async function enableMocking() {
-  if (!import.meta.env.DEV) {
-    return
-  }
   const { worker } = await import('./mocks/browser')
-  return worker.start()
+  return worker.start({ onUnhandledRequest: 'bypass' })
 }
 
 enableMocking().then(() => {
